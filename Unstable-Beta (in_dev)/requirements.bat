2d769ac7b8546dd20acd481cb616f09c78467bf3
Michi The Cat <mrzombsoriginal@gmail.com>
2026-02-09 23:09:41 +0300

    More Native
    Более удобный requirements через .bat

---
+++ Unstable-Beta (in_dev)/requirements.bat
@@ -0,0 +1,9 @@
+@echo off
+color 0a
+title AI friend - python requirements
+chcp 65001 > nul
+pip install ollama requests beautifulsoup4 wikipedia search-web websearch-python
+echo --------------------------------------------
+echo  Всё установлено! Если не вылезла ошибка...
+echo --------------------------------------------
+pause
